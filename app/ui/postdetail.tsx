48cca15f95574754c1646cedf64841b2ffd66f28
Refa Andhika <refandika@gmail.com>
2024-10-13 23:15:29 +0700

    Update rich text tags style

--- app/ui/postdetail.tsx
+++ app/ui/postdetail.tsx
@@ -62,7 +62,7 @@ const PostDetail = ({ slug }: { slug: string }) => {
             <div className="text-xl lg:text-2xl">{article.category.name}</div>
           </div>
           <div className="text-lg lg:text-xl my-4 flex flex-col gap-4 rich-text" dangerouslySetInnerHTML={{ __html: article.content }}></div>
-          <div className="text-lg lg:text-xl font-bold flex-row flex-wrap gap-4">
+          <div className="text-lg lg:text-xl font-bold flex flex-row flex-wrap gap-4">
             {tags?.map((tag: any) => (
               <div className="px-2 border border-gray-500 w-fit" key={tag.id}>{tag}</div>
             ))}
